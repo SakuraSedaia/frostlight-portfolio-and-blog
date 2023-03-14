800e75ff9e8f62a51497895ba7d7d5f8912db9f7
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2023-03-14 13:17:04 -0500

    Updated Development Page.
    Updated Navigation, replaced "Home" with "About Us"

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 function openInternal(url) {
     window.open(url,"_self");
 }
+
+function openExternal(url, trusted) {
+    if (trusted != 1) {
+        if (window.confirm("You are about to be directed to an external site, wish to continue?\n\nSite Link: " + url)) {
+            window.open(url, "_blank");
+        }
+    } else {
+        window.open(url, "_blank");
+    }
+}
